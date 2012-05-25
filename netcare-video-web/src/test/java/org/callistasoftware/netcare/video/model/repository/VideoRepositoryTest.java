3ec1d16708d55199511b9debf6d8829f9021b9cf
krantz.marcus@gmail.com <krantz.marcus@gmail.com>
2012-05-25 12:07:53 +0200

    Fix broken test.

--- netcare-video-web/src/test/java/org/callistasoftware/netcare/video/model/repository/VideoRepositoryTest.java
+++ netcare-video-web/src/test/java/org/callistasoftware/netcare/video/model/repository/VideoRepositoryTest.java
@@ -14,12 +14,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath:/netcare-video-config.xml")
+@ActiveProfiles(profiles={"test", "db-embedded"})
 public class VideoRepositoryTest {
 
 	@Autowired private CareUnitRepository cuRepo;
